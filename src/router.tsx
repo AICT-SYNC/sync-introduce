1eb804ad66e1c5159192d11b65a79d7a8fee87a0
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-24 03:21:14 +0900

    chore: remove BrowserRouter

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Desktop from "./page/Desktop";
 import Download from "./page/Download";
 import Product from "./page/Product";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/product" element={<Product />} />
-        <Route path="/desktop" element={<Desktop />} />
-        <Route path="/download" element={<Download />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/product" element={<Product />} />
+      <Route path="/desktop" element={<Desktop />} />
+      <Route path="/download" element={<Download />} />
+    </Routes>
   );
 };
 
