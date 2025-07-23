f10fa35196cc6bb726eb1464099a23dc8dfd44e9
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-07-24 03:18:56 +0900

    chore: add basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import Router from "./router";
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(<Router />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/sync-introduce/">
+    <Router />
+  </BrowserRouter>
+);
