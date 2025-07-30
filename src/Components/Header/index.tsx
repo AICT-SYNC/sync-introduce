13439190e33bebfc57a610dfa88bc4a408cf16d7
dlrjsgml44 <leegeh1213@gmail.com>
2025-07-30 21:08:27 +0900

    feat: Sync With Waitinglist

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -14,7 +14,7 @@ const Header = () => {
           <S.MenuItem onClick={() => navigate("/desktop")}>데스크톱</S.MenuItem>
           <S.MenuItem onClick={() => navigate("/download")}>다운로드</S.MenuItem>
         </S.Menus>
-        <S.StartSyncBox href="https://www.sync-saas.com" target="_self">
+        <S.StartSyncBox onClick={() => navigate("/waitinglist")}>
           <S.StartSyncText>Sync 무료로 사용하기</S.StartSyncText>
         </S.StartSyncBox>
       </S.Container>
