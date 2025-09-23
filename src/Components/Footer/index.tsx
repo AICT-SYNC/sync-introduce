7a3a9c8aeaf8aad126e3a7da0c99a026fc233f3f
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-09-24 08:37:13 +0900

    refactor: info

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -18,28 +18,27 @@ const Footer = () => {
             <S.LinkColumn>
               <S.ColumnTitle>제품</S.ColumnTitle>
               <S.LinkItem href="/product" onClick={() => navigate("/product")}>제품 소개</S.LinkItem>
-              <S.LinkItem href="/download" onClick={() => navigate("/download")}>다운로드</S.LinkItem>
-              <S.LinkItem href="/waitinglist" onClick={() => navigate("/waitinglist")}>Sync 사용하기</S.LinkItem>
+              <S.LinkItem href="/waitinglist" onClick={() => navigate("/waitinglist")}>Sync 베타테스트 신청</S.LinkItem>
             </S.LinkColumn>
             
             <S.LinkColumn>
               <S.ColumnTitle>지원</S.ColumnTitle>
               <S.LinkItem href="notify@sync-saas.com">고객 지원</S.LinkItem>
               <S.LinkItem href="notify@sync-saas.com">문의하기</S.LinkItem>
-              <S.LinkItem href="/help">도움말</S.LinkItem>
+              {/* <S.LinkItem href="/help">도움말</S.LinkItem> */}
             </S.LinkColumn>
             
-            <S.LinkColumn>
+            {/* <S.LinkColumn>
               <S.ColumnTitle>회사</S.ColumnTitle>
               <S.LinkItem href="/about">회사 소개</S.LinkItem>
               <S.LinkItem href="/privacy">개인정보처리방침</S.LinkItem>
               <S.LinkItem href="/terms">이용약관</S.LinkItem>
-            </S.LinkColumn>
+            </S.LinkColumn> */}
           </S.LinksSection>
         </S.TopSection>
         
         <S.BottomSection>
-          <S.Copyright>© 2024 Sync. All rights reserved.</S.Copyright>
+          <S.Copyright>© 2025 Sync. All rights reserved.</S.Copyright>
           <S.SocialLinks>
             <S.SocialLink href="https://github.com/AICT-SYNC" target="_blank">GitHub</S.SocialLink>
             <S.SocialLink href="notify@sync-saas.com">Email</S.SocialLink>
