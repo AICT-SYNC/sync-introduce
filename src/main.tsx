d996890ea52b5481e7e7a7233033822de6cd9cd0
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-09-24 08:38:04 +0900

    feat: add SyncThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,24 @@ import "./global.css";
 import Router from "./router";
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-
-
+import { SyncThemeProvider } from 'sync-design-system';
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={"/"}>
-    <Router />
-    <ToastContainer
-      position="top-right"
-      autoClose={2500}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick={false}
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-
-    />
-  </BrowserRouter>
+  <SyncThemeProvider theme={window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'}>
+    <BrowserRouter basename={"/"}>
+      <Router />
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </BrowserRouter>
+  </SyncThemeProvider>
 );
